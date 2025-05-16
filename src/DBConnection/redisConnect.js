6c439f976f8dbf20500680e46154ad8352e73f1b
kinjalweetechl <[kinjalweetech@gmail.com]>
2025-05-16 18:45:54 +0530

    first commit

--- src/DBConnection/redisConnect.js
+++ src/DBConnection/redisConnect.js
@@ -4,7 +4,7 @@ import { createClient } from "redis";
 dotenv.config();
 
 export const redis = createClient({
-    host: process.env.REDIS_HOST || "localhost",
+    host: process.env.REDIS_URL || "localhost",
     port: Number(process.env.REDIS_PORT) || 6379,
     // password: process.env.REDIS_PASSWORD,
     socket: {
@@ -23,6 +23,29 @@ export const redisConnect = async () => {
         throw error;
     }
 };
+// import dotenv from "dotenv";
+// import { createClient } from "redis";
+
+// dotenv.config();
+
+// export const redis = createClient({
+//     url: process.env.REDIS_URL,
+//     socket: {
+//         connectTimeout: 10000,
+//         reconnectStrategy: (retries) => Math.min(retries * 50, 1000),
+//     },
+// });
+
+// export const redisConnect = async () => {
+//     try {
+//         await redis.connect();
+//         await redis.ping();
+//         console.log("✅ Redis Database Connected Successfully");
+//     } catch (error) {
+//         console.error("❌ Error in Redis Connection:", error);
+//         throw error;
+//     }
+// };
 
 export const redisDisconnect = async () => {
     try {
--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ export const app = express();
 dotenv.config();
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: "*" ,//'http://localhost:5173' , 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
