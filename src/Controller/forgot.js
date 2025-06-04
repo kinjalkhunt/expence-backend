b4c78e54839b57733387ef99acc40b52a85c6fc1
kinjalweetechl <[kinjalweetech@gmail.com]>
2025-06-04 16:49:46 +0530

    first commit

--- src/Controller/forgot.js
+++ src/Controller/forgot.js
@@ -147,3 +147,36 @@ export const registerUser = async (req, res) => {
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+const generateToken = require('../utils/generateToken');
+
+exports.registerUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  const existing = await User.findOne({ username });
+  if (existing) return res.status(400).json({ message: 'User already exists' });
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({ username, password: hashedPassword });
+
+  res.status(201).json({ message: 'User registered', userId: user._id });
+};
+
+exports.loginUser = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  if (user && await bcrypt.compare(password, user.password)) {
+    res.json({ token: generateToken(user._id) });
+  } else {
+    res.status(401).json({ message: 'Invalid credentials' });
+  }
+};
+
+exports.getProfile = async (req, res) => {
+  const user = await User.findById(req.user.id).select('-password');
+  res.json(user);
+};
